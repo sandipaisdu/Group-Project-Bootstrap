939d4f153788267a29d080610196052a3721a7f7
Sandipaisdu <ksandipdu@gmail.com>
2015-06-23 04:16:08 +0600

    FInal

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// jQuery(document).ready(function(){
+	// jQuery('#nav').slicknav();
+// });
+
+
+jQuery(document).ready(function(){
+
+	// Instantiate MixItUp:
+
+	jQuery('#project_action').mixItUp();
+
+});
